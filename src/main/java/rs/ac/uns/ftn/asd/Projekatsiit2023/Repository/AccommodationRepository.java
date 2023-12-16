ed8768a0ce4305af89822a2bdcd5c798298c8dad
Stevan Sapun <stevan.sapun@gmail.com>
2023-12-16 19:16:19 +0100

    Adated the search and filter queries to the new Accommodation model.

--- src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/Repository/AccommodationRepository.java
+++ src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/Repository/AccommodationRepository.java
@@ -12,41 +12,37 @@ import java.util.Set;
 import java.util.UUID;
 @Repository
 public interface AccommodationRepository extends JpaRepository<Accommodation, UUID> {
-//    @Query("SELECT a FROM Accommodation a " +
-//            "WHERE a.location.city = :city " +
-//            "AND :guestNumber BETWEEN a.minGuests AND a.maxGuests " +
-//            "AND EXISTS (SELECT 1 FROM AccommodationReservedDates ar " +
-//            "JOIN ar.reservations r " +
-//            "WHERE r.accommodationReservedDates = ar " +
-//            "AND ar = a.availability " + // adjust this line
-//            "AND r.datePeriod.startDate <= :endDate " +
-//            "AND r.datePeriod.endDate >= :startDate)")
-//    List<Accommodation> searchAccommodations(
-//            @Param("city") String city,
-//            @Param("guestNumber") int guestNumber,
-//            @Param("startDate") LocalDate startDate,
-//            @Param("endDate") LocalDate endDate);
-//
-//    @Query("SELECT a FROM Accommodation a " +
-//            "WHERE a.location.city = :city " +
-//            "AND :guestNumber BETWEEN a.minGuests AND a.maxGuests " +
-//            "AND EXISTS (SELECT 1 FROM AccommodationReservedDates ar " +
-//            "JOIN ar.reservations r " +
-//            "WHERE r.accommodationReservedDates = ar " +
-//            "AND ar = a.availability " + // Adjusted line
-//            "AND r.datePeriod.startDate <= :endDate " +
-//            "AND r.datePeriod.endDate >= :startDate) " +
-//           // "AND (:amenities IS EMPTY OR :amenities IN a.amenities) " +
-//            "AND (:accommodationType IS NULL OR a.type = :accommodationType) " +
-//            "AND (:minPrice IS NULL OR a.price >= :minPrice) " +
-//            "AND (:maxPrice IS NULL OR a.price <= :maxPrice)")
-//    List<Accommodation> filterAccommodations(
-//            @Param("city") String city,
-//            @Param("guestNumber") int guestNumber,
-//            @Param("startDate") LocalDate startDate,
-//            @Param("endDate") LocalDate endDate,
-//            //@Param("amenities") List<String> amenities,
-//            @Param("accommodationType") String accommodationType,
-//            @Param("minPrice") Double minPrice,
-//            @Param("maxPrice") Double maxPrice);
+    @Query("SELECT a FROM Accommodation a " +
+            "WHERE a.location.city = :city " +
+            "AND :guestNumber BETWEEN a.minGuests AND a.maxGuests " +
+            "AND EXISTS (SELECT 1 FROM AccommodationDatePeriod adp " +
+            "WHERE adp.accommodation = a " +
+            "AND adp.startDate <= :endDate " +
+            "AND adp.endDate >= :startDate)") //ako treba da se menja logika samo se zamene <= i >=
+    List<Accommodation> searchAccommodations(
+            @Param("city") String city,
+            @Param("guestNumber") int guestNumber,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
+
+    @Query("SELECT a FROM Accommodation a " +
+            "WHERE a.location.city = :city " +
+            "AND :guestNumber BETWEEN a.minGuests AND a.maxGuests " +
+            "AND EXISTS (SELECT 1 FROM AccommodationDatePeriod adp " +
+            "WHERE adp.accommodation = a " +
+            "AND adp.startDate <= :endDate " +
+            "AND adp.endDate >= :startDate) " +
+            "AND (:accommodationType IS NULL OR a.type = :accommodationType) " +
+            "AND (:minPrice IS NULL OR a.price >= :minPrice) " +
+            "AND (:maxPrice IS NULL OR a.price <= :maxPrice) " +
+            "AND (:amenities IS EMPTY OR a.amenities IN :amenities)")
+    List<Accommodation> filterAccommodations(
+            @Param("city") String city,
+            @Param("guestNumber") int guestNumber,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("amenities") List<String> amenities,
+            @Param("accommodationType") String accommodationType,
+            @Param("minPrice") Double minPrice,
+            @Param("maxPrice") Double maxPrice);
 }
