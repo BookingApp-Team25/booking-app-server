817cb76f96ef1c776411ddddcc369e203690b316
Miroslav0222 <105170901+Miroslav0222@users.noreply.github.com>
2024-01-11 14:02:58 +0100

    Minor bug fix

---
+++ src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/dto/AccommodationLog.java
@@ -0,0 +1,11 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023.dto;
+
+import java.util.UUID;
+
+public class AccommodationLog {
+    UUID accommodationId; //id treba jer kad se na apartman klikne moracu da za njega dobavim dodatno
+    String accommodationName;
+    String accommodationPhoto;
+    int reservationNumber;
+    double totalProfit;
+}
---
+++ src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/dto/AccommodationLogCollection.java
@@ -0,0 +1,8 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023.dto;
+
+import java.util.Collection;
+
+public class AccommodationLogCollection {
+    Collection<AccommodationLog> logs;
+    int totalNumberOfElements;
+}
