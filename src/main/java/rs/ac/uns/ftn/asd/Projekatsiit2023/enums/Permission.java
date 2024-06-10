60811b9d116f38195d035ef802130408a5ba6dd8
SavaMander <104906323+SavaMander@users.noreply.github.com>
2024-06-10 10:24:02 +0200

    Implemented security features

---
+++ src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/enums/Permission.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023.enums;
+
+public enum Privilege {
+    Edit_Accommodation_Update,
+    Read_Accommodation_Update,
+    Create_Accommodation_Update,
+    Resolve_Accommodation_Update,
+    Create_Reservation,
+    Cancel_Reservation,
+    Read_Accommodation,
+    Read_Guest
+}
